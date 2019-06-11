fec92cbc767f5b19a13efcc82da84909d2f252d7
joshrevell4 <51717755+joshrevell4@users.noreply.github.com>
2019-06-11 13:51:28 -0400

    Add files via upload

---
+++ ocTest/src/main/java/ocTest/OcTest.java
@@ -0,0 +1,26 @@
+package ocTest;
+
+import java.util.Scanner;
+
+public class OcTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Application is running...");
+		
+		boolean isRunning = true;
+		Scanner scanner = new Scanner(System.in);
+		String input;
+		
+		while(isRunning) {
+			System.out.println("quit? y/n: ");
+			input = scanner.nextLine();
+			if(input.equals("y")) {
+				isRunning = false;
+			}
+		}
+		
+		scanner.close();
+		System.out.println("Bye");
+	}
+
+}
